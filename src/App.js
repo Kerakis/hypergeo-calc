971ec3f014438b0358bd304210116b0b3cce4c18
Rob Upchurch <rlupchurch3@gmail.com>
2022-03-20 15:07:22 -0400

    Fixed a typo

--- src/App.js
+++ src/App.js
@@ -84,7 +84,7 @@ export default function App() {
             This hypergeometric distribution calculator can help you determine
             the probabilities of you drawing a specific card or cards from a
             library of cards. For example, it can help you determine the
-            liklihood of drawing 2 land cards in your opening hand from a
+            likelihood of drawing 2 land cards in your opening hand from a
             library of 100 cards which contains 40 land cards.
           </h3>
         </div>
